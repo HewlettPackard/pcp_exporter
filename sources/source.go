191696a99075f62c2305c069c70082b4f4875aac
Kalyan Mada <kalyanmada97@gmail.com>
2017-07-25 15:18:29 -0400

    Cleaned up the golint errors Signed-Off-By: Kalyan Mada <kalyan.red.mada@hpe.com>

--- sources/source.go
+++ sources/source.go
@@ -4,10 +4,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+//Namespace constant
 const Namespace = "pcp"
 
+//Factories variable to create a map
 var Factories = make(map[string]func() (PcpSource, error))
 
+//PcpSource interface
 type PcpSource interface {
 	Update(ch chan<- prometheus.Metric) (err error)
 }
